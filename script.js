cd94582609893d54f78e394b25db4ffbc6bfcec4
Ray-tto <anessantana0@gmail.com>
2024-10-22 23:13:45 +0100

    .

--- script.js
+++ script.js
@@ -1,10 +1,69 @@
-function menuShow(){
-    let menuMobile = document.querySelector('.menu-mobile');
-    if (menuMobile.classList.contains('open')){
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "https://img.icons8.com/ios-filled/30/ffffff/menu--v6.png";
-    } else{
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "https://img.icons8.com/ios-glyphs/30/ffffff/delete-sign.png";
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active'); 
+    navbar.classList.remove('active'); 
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active'); 
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active'); 
+    navbar.classList.remove('active'); 
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active')
+    }else{
+        document.querySelector('.header').classList.remove('active')
     }
 }
+
+window.onload = () =>{
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active')
+    }else{
+        document.querySelector('.header').classList.remove('active')
+    }
+}
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    centeredSlides: true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+  });
+  
+  var swiper = new Swiper(".products-slider", {
+    spaceBetween: 20,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    grabCursor:true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+    },
+  });
